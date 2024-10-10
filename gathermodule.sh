a7898863424aba645e8feb6c537caeed0c414c76
Diep Nguyen <diep90.nt@gmail.com>
2024-10-10 12:12:00 +0700

    Update gathermodule.sh

--- gathermodule.sh
+++ gathermodule.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-# Define the CSV output file
-output_file="dll_info.csv"
+# Find the first PID of the process running '/usr/share/dotnet/dotnet'
+pid=$(/tools/dotnet-dump ps | grep '/usr/share/dotnet/dotnet' | awk '{print $1}' | head -n 1)
+
+# Check if PID is found
+if [ -z "$pid" ]; then
+  echo "No process found for '/usr/share/dotnet/dotnet'."
+  exit 1
+fi
+
+# Extract the environment variables of the process
+environ=$(cat "/proc/$pid/environ" | tr '\0' '\n')
+
+# Extract WEBSITESITENAME from the environment variable
+WEBSITESITENAME=$(echo "$environ" | grep '^WEBSITE_SITE_NAME=' | cut -d= -f2)
+
+# Check if WEBSITESITENAME is found
+if [ -z "$WEBSITESITENAME" ]; then
+  echo "WEBSITESITENAME not found in environment."
+  exit 1
+fi
+
+# Get the current date in YYYYMMDD format
+current_date=$(date +"%Y%m%d")
+
+# Define the CSV output file with WEBSITESITENAME and current date
+output_file="${WEBSITESITENAME}_${current_date}.csv"
 
 # Create or overwrite the CSV file with headers
 echo "module_name,modified_date,current_version,newest_version,tag,links,notes" > "$output_file"
